8016e4a9408237afdcd1bfa93bc5a8ea70fde831
Kim Min JU <38_0613@naver.com>
2023-07-24 23:09:28 +0900

    8장

---
+++ src/main/java/com/minju/springbootdeveloper/service/UserDetailService.java
@@ -0,0 +1,20 @@
+package com.minju.springbootdeveloper.service;
+
+import com.minju.springbootdeveloper.domain.User;
+import com.minju.springbootdeveloper.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public User loadUserByUsername(String email){
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new IllegalArgumentException((email)));
+    }
+}
